a2487651ab42f0bbfea2a4cbee8c7e3faeb95bcc
kingoker <57072347+kingoker@users.noreply.github.com>
2021-09-27 16:11:02 +0500

    Update config.py

--- data/config.py
+++ data/config.py
@@ -6,7 +6,7 @@ env.read_env()
 
 # .env fayl ichidan quyidagilarni o'qiymiz
 BOT_TOKEN = "2045379423:AAHQu_US3v4BFtaUSxZjbZSi8XSVHMasiGA"  # Bot toekn
-ADMINS = ["1600170280, "]  # adminlar ro'yxati
+ADMINS = ["1600170280, 3513638, 1019865273"]  # adminlar ro'yxati
 IP = "localhost"  # Xosting ip manzili
 # account_sid = 'AC7586a0983c8b415abe41f7a803c5e61a' # Twilio ma'lumotlar
 # auth_token = '1b6e0f6bf10df049c0b68ee44dbeedc8'
