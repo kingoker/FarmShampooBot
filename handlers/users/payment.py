f0c8d93dfbae9005a00c81e3c985ae16b01f40c3
kingoker <57072347+kingoker@users.noreply.github.com>
2021-09-28 22:28:54 +0500

    Update payment.py

--- handlers/users/payment.py
+++ handlers/users/payment.py
@@ -9,45 +9,68 @@ from keyboards.default import amount_menu_uz, amount_menu_eng, products_menu_uz,
 from states.Customer_state import Customer_Form
 from aiogram.dispatcher import FSMContext
 from utils.misc.show_gmap import show
+from utils import admin_send_message, mahsulot_yuborish
 from data.config import  PAYMENTS_PROVIDER_TOKEN, ADMINS
 
 
+@dp.message_handler(lambda message:message.text=="⬅️Ortga", state=Customer_Form.delivery)
+async def back_uz(message : types.Message, state : FSMContext):
+    user_id = message.from_user.id
+    customer = session.query(Customer).filter(Customer.customer_id==user_id).first()
+    customer.products.clear()
+    session.commit()
+    text = "Bosh menyu"
+    await state.reset_state()
+    await message.answer(text, reply_markup=menu_product_types_uz)
 
-async def admin_send_message(message, customer, pickup=False, delivery=False, paid=False, cash=False):
-  if customer.yuborish_turi == "🚖Доставка":
-    admin_text = f"<strong>🚖 Новый заказ</strong> – Доставка:\n"
-  else:
-    admin_text = f"🏃 <strong>Новый заказ</strong> – Самовывоз:\n"
-  total_price = 0
-  admin_text += f"<strong>Юзер</strong>:@{message.from_user.username}\n"
-  admin_text += f"<strong>Имя</strong>:{customer.username}\n"
-  admin_text += f"<strong>Номер телефона</strong>: {customer.phone}\n"
-  admin_text += f"<strong>Язык</strong>: {customer.language}\n"
-  i = 0
-  records = session.query(savat, Customer).filter(Customer.customer_id==customer.customer_id, savat.c.customer_id == customer.customer_id).all()
-  for row in records:
-    product = session.query(Product).filter(Product.product_id==row.product_id).first()
-    i +=1
-    admin_text += f"<strong>{i}. {product.title}</strong>\n\n"
-    total_price += int(row.amount) * int(product.price)
-    price = format(int(product.price),",d").replace(',', ' ')
-    amount_show = f"{int(row.amount) * int(product.price):,}".replace(',', ' ')
-    admin_text+= f"{row.amount} x {price} = {amount_show} UZS\n\n"
-  total_price = f"{total_price:,}".replace(',', ' ')
-  admin_text += f"<strong>Сумма оплаты: </strong> {total_price} UZS\n"
-  if cash:
-    admin_text += f"<strong>Cпособ оплаты</strong>: Наличные\n"
-  if not paid:
-    admin_text += f"<strong>Оплачено</strong>: ⛔️\n"
-  else:
-    admin_text += f"<strong>Оплачено</strong>: ✅\n"
-  if customer.yuborish_turi == "🚖Доставка":
-    admin_text += f"<strong>Адрес</strong>: {show(customer.latitude, customer.longitude)}\n"   
-  admin_text += f"<strong>Время: </strong> {customer.time}\n"
-  admin_text += f"<strong>Комментарии</strong>:{customer.comment}"
-  for admin in ADMINS:
-    try:
-      await dp.bot.send_message(admin, admin_text)
 
-    except Exception as err:
-      logging.exception(err)
+@dp.message_handler(lambda message:message.text=="⬅️Назад", state=Customer_Form.delivery)
+async def back_eng(message : types.Message, state : FSMContext):
+    user_id = message.from_user.id
+    customer = session.query(Customer).filter(Customer.customer_id==user_id).first()
+    customer.products.clear()
+    session.commit()
+    text = "Главное меню"
+    await state.reset_state()
+    await message.answer(text, reply_markup=menu_product_types_eng)
+
+
+
+
+@dp.message_handler(state=Customer_Form.delivery)
+async def delivery(message : types.Message, state : FSMContext):
+    user_id = message.from_user.id
+    customer = session.query(Customer).filter(Customer.customer_id==user_id).first()
+    lang = "uz" if customer.language == "🇺🇿O'zbekcha" else "eng"
+    records = session.query(savat, Customer).filter(Customer.customer_id==customer.customer_id, savat.c.customer_id==customer.customer_id).all()
+    if lang == "uz":
+        description = "To'lov qilish uchun quyidagi tugmani bosing."
+    else :
+        description = "Нажмите кнопку ниже, чтобы заплатить."    
+    await mahsulot_yuborish(message, description, records, customer)
+    await state.reset_state()
+
+
+
+@dp.pre_checkout_query_handler(lambda query: True)
+async def checkout(pre_checkout_query: types.PreCheckoutQuery):
+    await bot.answer_pre_checkout_query(pre_checkout_query.id, ok=True,
+                                        error_message="Aliens tried to steal your card's CVV,"
+                                                      " but we successfully protected your credentials,"
+                                                      " try to pay again in a few minutes, we need a small rest.")
+
+
+@dp.message_handler(content_types=ContentTypes.SUCCESSFUL_PAYMENT)
+async def got_payment(message: types.Message):
+    user_id = message.from_user.id
+    customer = session.query(Customer).filter(Customer.customer_id == user_id).first()
+    print(customer.products)
+    customer.products.clear()
+    await admin_send_message(message=message, customer=customer, paid=True)
+    customer.latitude = None
+    customer.longitude = None
+    session.commit()    
+    lang = "uz" if customer.language == "🇺🇿O'zbekcha" else "eng"
+    text = {"uz":"Xaridingiz uchun rahmat.", "eng" : "Спасибо за покупку."}
+    keyboard = menu_product_types_uz if lang == "uz" else menu_product_types_eng    
+    await message.answer(text[lang],reply_markup=keyboard)
